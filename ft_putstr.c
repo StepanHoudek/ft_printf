af6707345568ea262240004ab09642c8571d0ef2
shoudek <stepan.houdek1@gmail.com>
2024-01-18 17:12:29 +0100

    Fix: ft_putstr

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: shoudek <shoudek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 14:22:54 by shoudek           #+#    #+#             */
-/*   Updated: 2024/01/18 15:54:27 by shoudek          ###   ########.fr       */
+/*   Updated: 2024/01/18 16:12:24 by shoudek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_putstr(char *s, int *ptr_sum)
 {
+	if (s == NULL)
+		return ;
 	while (*s)
 		ft_putchar(*s++, ptr_sum);
 }
