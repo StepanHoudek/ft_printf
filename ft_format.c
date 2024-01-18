e54ef034d55862b6d8d0fb0588bcd02080524ce6
shoudek <stepan.houdek1@gmail.com>
2024-01-18 17:12:16 +0100

    Add: ft_format

---
+++ ft_format.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_format.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shoudek <shoudek@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 16:16:59 by shoudek           #+#    #+#             */
+/*   Updated: 2024/01/18 16:21:14 by shoudek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_format(char *format, int *ptr_sum, va_list args)
+{
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			ft_putchar(*format++, ptr_sum);
+			continue ;
+		}
+		if (*(format + 1) == '%' && *format == '%')
+			ft_putchar('%', ptr_sum);
+		if (*(format + 1) == 'c')
+			ft_putchar(va_arg(args, int), ptr_sum);
+		if (*(format + 1) == 's')
+			ft_putstr(va_arg(args, char *), ptr_sum);
+		if (*(format + 1) == 'd' || *(format + 1) == 'i')
+			ft_putnbr(va_arg(args, int), ptr_sum);
+		format = format + 2;
+	}
+	return ;
+}
