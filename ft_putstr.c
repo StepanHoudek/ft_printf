aa633c4d2282888a6ae18daee6f7162d49556017
shoudek <stepan.houdek1@gmail.com>
2024-01-23 13:49:28 +0100

    Fix: null

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: shoudek <shoudek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 14:22:54 by shoudek           #+#    #+#             */
-/*   Updated: 2024/01/18 16:12:24 by shoudek          ###   ########.fr       */
+/*   Updated: 2024/01/23 12:45:37 by shoudek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 void	ft_putstr(char *s, int *ptr_sum)
 {
 	if (s == NULL)
+	{
+		ft_putstr("(null)", ptr_sum);
 		return ;
+	}
 	while (*s)
 		ft_putchar(*s++, ptr_sum);
 }
